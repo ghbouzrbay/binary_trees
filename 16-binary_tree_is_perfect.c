370fff515e6c181c97861e631542b49239e3d725
bouzrbay <atighmarcelline@gmail.com>
2023-08-30 13:28:13 -0700

    code

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,40 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree - ...
+ *
+ * @p: pointer to the root node of the tree to check.
+ * @a_height: accumulated height of the current path in the tree.
+ * @count: pointer to the tree's leaf count value.
+ * @max: pointer to the tree's maximum height value.
+ *
+ */
+
 void binary_tree(const binary_tree_t *p, size_t a_height,
-		 size_t *count, size_t *max);
+		 size_t *count, size_t *max)
+{
+
+  if (p != NULL)
+    {
+
+      if ((p->left == NULL) && (p->right == NULL))
+	{
+
+	  if (count != NULL)
+	    (*count)++;
+
+	  if ((max != NULL) && (a_height > *max))
+	    a_height = *max;
+	}
+    }
+
+  else
+    {
+      binary_tree(p->left, a_height + 1, count, max);
+      binary_tree(p->right, a_height + 1, count, max);
+    }
+
+}
 
 /**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect.
@@ -21,38 +54,3 @@ binary_tree(tree, 0, &count, &height);
 return ((int)count == (1 << height) ? 1 : 0);
 }
 
-
-/**
- * binary_tree - ...
- *
- * @p: pointer to the root node of the tree to check.
- * @a_height: accumulated height of the current path in the tree.
- * @count: pointer to the tree's leaf count value.
- * @max: pointer to the tree's maximum height value.
- *
- */
-void binary_tree(const binary_tree_t *p, size_t a_height,
-		 size_t *count, size_t *max)
-{
-
-if (p != NULL)
-{
-
-if ((p->left == NULL) && (p->right == NULL))
-{
-
-if (count != NULL)
-(*count)++;
-
-if ((max != NULL) && (a_height > *max))
-a_height = *max;
-}
-}
-
-else
-{
-binary_tree(p->left, a_height + 1, count, max);
-binary_tree(p->right, a_height + 1, count, max);
-}
-
-}
